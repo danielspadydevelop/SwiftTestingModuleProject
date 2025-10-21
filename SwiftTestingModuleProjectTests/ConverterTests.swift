2d4155c52ffaea53ce071c67add047e058b27a31
Daniel Spady <danielspadyassurance22@gmail.com>
2025-10-21 19:56:59 -0400

    Update Test Data to make pass
    - Create Success test by updating data

--- SwiftTestingModuleProjectTests/ConverterTests.swift
+++ SwiftTestingModuleProjectTests/ConverterTests.swift
@@ -11,11 +11,11 @@ import Testing
 @Test func fahrenheitToCelsius() throws {
     let sut = Converter()
 
-    let input1 = 30.0
+    let input1 = 32.0
     let output1 = sut.convertToCelsius(fahrenheit: input1)
     #expect(output1 == 0)
 
-    let input2 = 210.0
+    let input2 = 212.0
     let output2 = sut.convertToCelsius(fahrenheit: input2)
     #expect(output2 == 100)
 }
