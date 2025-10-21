8932e1d545322c55ea01833c36bdf99a9ca36ef3
Daniel Spady <danielspadyassurance22@gmail.com>
2025-10-21 19:55:54 -0400

    Fail test setup
    - Create Converter struct
    - Add Tests using Testing Module
    - fail tests based on results

---
+++ SwiftTestingModuleProject/Converter.swift
@@ -0,0 +1,12 @@
+//
+//  Converter.swift
+//  SwiftTestingModuleProject
+//
+//  Created by Daniel Spady on 10/21/25.
+//
+
+struct Converter {
+    func convertToCelsius(fahrenheit: Double) -> Double {
+        (fahrenheit - 32) * 5 / 9
+    }
+}
---
+++ SwiftTestingModuleProjectTests/ConverterTests.swift
@@ -0,0 +1,21 @@
+//
+//  ConverterTests.swift
+//  SwiftTestingModuleProject
+//
+//  Created by Daniel Spady on 10/21/25.
+//
+
+import Testing
+@testable import SwiftTestingModuleProject
+
+@Test func fahrenheitToCelsius() throws {
+    let sut = Converter()
+
+    let input1 = 30.0
+    let output1 = sut.convertToCelsius(fahrenheit: input1)
+    #expect(output1 == 0)
+
+    let input2 = 210.0
+    let output2 = sut.convertToCelsius(fahrenheit: input2)
+    #expect(output2 == 100)
+}
