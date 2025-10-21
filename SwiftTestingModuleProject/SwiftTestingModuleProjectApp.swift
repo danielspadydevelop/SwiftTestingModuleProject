414e4abae431bb4df010e4aaf50625b16059c8cd
Daniel Spady <danielspadyassurance22@gmail.com>
2025-10-21 19:39:06 -0400

    Initial Commit

---
+++ SwiftTestingModuleProject/SwiftTestingModuleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingModuleProjectApp.swift
+//  SwiftTestingModuleProject
+//
+//  Created by Daniel Spady on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingModuleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
