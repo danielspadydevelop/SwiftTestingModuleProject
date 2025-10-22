770dc2372ea3eedd938f6e98d10f4b3480878bbe
Daniel Spady <danielspadyassurance22@gmail.com>
2025-10-22 18:27:24 -0400

    Handle Async Await Errors
    - Catch errors that are intermittent

--- SwiftTestingModuleProjectTests/GameTests.swift
+++ SwiftTestingModuleProjectTests/GameTests.swift
@@ -53,3 +53,19 @@ extension GameError: @retroactive CustomTestStringConvertible {
         }
     }
 }
+
+@Test func loadNames() async {
+    let viewModel = TestViewModel()
+
+    await withKnownIssue("Names can sometimes come back with too few values", isIntermittent: true) {
+        try await viewModel.loadNames()
+        #expect(viewModel.names.isEmpty == false, "Names should be full of values.")
+    }
+}
+
+struct TestViewModel {
+    var names: [String] = []
+    func loadNames() async throws {
+        
+    }
+}
