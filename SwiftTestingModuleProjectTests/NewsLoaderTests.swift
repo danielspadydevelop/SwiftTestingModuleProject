af0ec827fcefce6a4598c676632e3727c7eb61d6
Daniel Spady <danielspadyassurance22@gmail.com>
2025-11-01 00:04:58 -0400

    Add Confirmation Range for Network Request
    - Create Struct that makes a session call to a json
    - Write a unit test that confirms a range of 5 to 10 network calls
    - Grant Permission in App Sandbox to allow network calls
    - Add Approve Domain to NSExceptionDomains

---
+++ SwiftTestingModuleProjectTests/NewsLoaderTests.swift
@@ -0,0 +1,24 @@
+//
+//  NewsLoaderTests.swift
+//  SwiftTestingModuleProject
+//
+//  Created by Daniel Spady on 10/31/25.
+//
+
+@testable import SwiftTestingModuleProject
+
+import Testing
+
+@Suite("Confirmation Range Tests with NewsLoader", .timeLimit(.minutes(1)))
+struct ConfirmationRangeNewsLoaderTests {
+    @Test("At Least 5 to 10 feeds should be returned")
+    func fiveToTenFeedsAreLoaded() async throws {
+        let loader = NewsLoader()
+
+        await confirmation(expectedCount: 5...10) { confirm in
+            for await _ in loader {
+                confirm()
+            }
+       }
+    }
+}
